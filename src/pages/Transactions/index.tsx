a9aa9e2735f98689054846b906c7a08505feb83b
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-01 02:07:22 -0300

    feat: added transaction table styles and structure

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,43 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+    PriceHightlight,
+    TransacationsTable,
+    TransactionsContainer,
+} from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransacationsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento do site</td>
+                            <td>
+                                <PriceHightlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHightlight>
+                            </td>
+                            <td>venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHightlight variant="outcome">
+                                    - R$ 50,40
+                                </PriceHightlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransacationsTable>
+            </TransactionsContainer>
         </div>
     );
 }
