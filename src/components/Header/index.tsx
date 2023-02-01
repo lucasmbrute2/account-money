535b804e2eec972f5d31efa2f9299d2a17c022ea
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-01 02:41:51 -0300

    feat: structuring Radix dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,27 @@
 import { HeaderContent, HeaderContinar, NewTransactionButton } from "./styles";
 import logoImage from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
     return (
         <HeaderContinar>
             <HeaderContent>
                 <img src={logoImage} alt="" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>
+                            Nova transação
+                        </NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <Dialog.Portal>
+                        <Dialog.Overlay />
+                        <Dialog.Content>
+                            <Dialog.Title>Nova transação</Dialog.Title>
+                            <Dialog.Close />
+                        </Dialog.Content>
+                    </Dialog.Portal>
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContinar>
     );
